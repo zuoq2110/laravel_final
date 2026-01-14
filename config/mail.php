b9878459230b30fed96114d40c3c2d91a3c75f9c
duong <zuoq2110@gmail.com>
2026-01-14 11:09:20 +0700

    update

--- config/mail.php
+++ config/mail.php
@@ -48,6 +48,16 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
+        'sendgrid' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.sendgrid.net'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+        ],
+
 
         'ses' => [
             'transport' => 'ses',
