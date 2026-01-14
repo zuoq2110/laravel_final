265f5311f2a0546219e44dcd905105bc5481fb76
duong <zuoq2110@gmail.com>
2026-01-14 11:26:35 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                             href="{{ route('login') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
                         >
-                            Log in
+                            Đăng nhập
                         </a>
 
                         @if (Route::has('register'))
